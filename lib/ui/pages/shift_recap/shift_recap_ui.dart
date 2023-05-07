fbb9c16efa0371cd2f0bfbcf2bfd2621daf79ebb
Novian Andika <hello@fleetime.my.id>
2023-05-08 05:52:39 +0700

    ✨ feat(shift_recap_ui.dart): add ShiftRecapPage widget This commit adds a new widget, ShiftRecapPage, which is a StatefulWidget that displays a Placeholder widget.

---
+++ lib/ui/pages/shift_recap/shift_recap_ui.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class ShiftRecapPage extends StatefulWidget {
+  const ShiftRecapPage({super.key});
+
+  @override
+  State<ShiftRecapPage> createState() => _ShiftRecapPageState();
+}
+
+class _ShiftRecapPageState extends State<ShiftRecapPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
