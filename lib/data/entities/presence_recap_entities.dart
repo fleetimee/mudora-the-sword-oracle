a033371527e06f9549e1f39a7ebe214a3144781b
Novian Andika <novian.andika@students.amikom.ac.id>
2023-05-09 15:08:34 +0700

    🎨 style(presence_recap_entities.dart): add presence recap entities ✨ feat(main.dart): replace SalaryDetailPage with PresenceRecapPage 🖼️ add(home_pulangg.png): add home_pulangg.png image The PresenceRecapEntities class is added to the presence_recap_entities.dart file. This class is used to store the presence recap data. The main.dart file is updated to use the PresenceRecapPage instead of the SalaryDetailPage. The home_pulangg.png image is added to the assets/images directory.
    🎉 feat(presence_recap_ui.dart): add presence recap page UI
    🚀 chore(pubspec.yaml): add syncfusion_flutter_charts dependency
    The presence recap page UI is added, which displays a chart and a list of presence records. The chart displays the percentage of attendance and the list displays the date, time, and status of each presence record. The syncfusion_flutter_charts dependency is added to the pubspec.yaml file to support the chart display.

---
+++ lib/data/entities/presence_recap_entities.dart
@@ -0,0 +1,54 @@
+class PresenceRecapEntities {
+  final String date;
+  final String time;
+  final String status;
+
+  PresenceRecapEntities({
+    required this.date,
+    required this.time,
+    required this.status,
+  });
+}
+
+final List<PresenceRecapEntities> presenceRecapEntitiesList = [
+  PresenceRecapEntities(
+    date: 'Senin, 20 Juli',
+    time: '08:00',
+    status: 'Hadir',
+  ),
+  PresenceRecapEntities(
+    date: 'Senin, 20 Juli',
+    time: '08:00',
+    status: 'Pulang',
+  ),
+  PresenceRecapEntities(
+    date: 'Senin, 20 Juli',
+    time: '08:00',
+    status: 'Hadir',
+  ),
+  PresenceRecapEntities(
+    date: 'Senin, 20 Juli',
+    time: '08:00',
+    status: 'Pulang',
+  ),
+  PresenceRecapEntities(
+    date: 'Senin, 20 Juli',
+    time: '08:00',
+    status: 'Hadir',
+  ),
+  PresenceRecapEntities(
+    date: 'Senin, 20 Juli',
+    time: '08:00',
+    status: 'Pulang',
+  ),
+  PresenceRecapEntities(
+    date: 'Senin, 20 Juli',
+    time: '08:00',
+    status: 'Pulang',
+  ),
+  PresenceRecapEntities(
+    date: 'Senin, 20 Juli',
+    time: '08:00',
+    status: 'Pulang',
+  ),
+];
---
+++ lib/ui/pages/presence_recap/presence_recap_ui.dart
@@ -0,0 +1,197 @@
+import 'package:fleetime_hris/common/constant/color_constant.dart';
+import 'package:fleetime_hris/data/entities/presence_recap_entities.dart';
+import 'package:fleetime_hris/ui/widget/appbar.dart';
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+class PresenceRecapPage extends StatefulWidget {
+  const PresenceRecapPage({super.key});
+
+  @override
+  State<PresenceRecapPage> createState() => _PresenceRecapPageState();
+}
+
+class _PresenceRecapPageState extends State<PresenceRecapPage> {
+  late List<_ChartData> data;
+
+  @override
+  void initState() {
+    data = [
+      _ChartData('David', 20, AppColors.stateErrorBase),
+      _ChartData('Steve', 80, AppColors.stateSuccessBase),
+    ];
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBarWithBack(
+        title: 'Rekap Absensi',
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                SfCircularChart(
+                  series: <CircularSeries>[
+                    DoughnutSeries<_ChartData, String>(
+                      innerRadius: '70%',
+                      dataSource: data,
+                      radius: '80%',
+                      xValueMapper: (_ChartData data, _) => data.x,
+                      yValueMapper: (_ChartData data, _) => data.y,
+                      pointColorMapper: (_ChartData data, _) =>
+                          data.pointColorMapper,
+                    )
+                  ],
+                ),
+                Positioned.fill(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        'Total Absensi',
+                        style: TextStyle(
+                          color: AppColors.grayscaleLabel,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        '95%',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        'Aktivitas',
+                        style: TextStyle(
+                          color: AppColors.grayscaleTitle,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      DropdownButton<String>(
+                        value: '2023',
+                        icon: const Icon(Icons.arrow_drop_down),
+                        iconSize: 24,
+                        elevation: 16,
+                        style: const TextStyle(
+                          color: AppColors.grayscaleLabel,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                        underline: Container(
+                          height: 2,
+                          color: AppColors.grayscaleTitle,
+                        ),
+                        onChanged: (String? newValue) {},
+                        items: <String>['2023', '2022', '2021']
+                            .map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                      )
+                    ],
+                  ),
+                  Expanded(
+                    child: ListView.separated(
+                      shrinkWrap: true,
+                      separatorBuilder: (context, index) {
+                        return const Divider();
+                      },
+                      itemCount: presenceRecapEntitiesList.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          contentPadding: const EdgeInsets.all(0),
+                          leading: presenceRecapEntitiesList[index].status ==
+                                  'Hadir'
+                              ? CircleAvatar(
+                                  backgroundColor:
+                                      Colors.green.withOpacity(0.15),
+                                  child: Image.asset(
+                                    'assets/images/home_datang.png',
+                                    fit: BoxFit.contain,
+                                    height: 24,
+                                  ),
+                                )
+                              : CircleAvatar(
+                                  backgroundColor: Colors.red.withOpacity(0.15),
+                                  child: Image.asset(
+                                    'assets/images/home_pulangg.png',
+                                    fit: BoxFit.contain,
+                                    height: 24,
+                                  ),
+                                ),
+                          title: Text(
+                            presenceRecapEntitiesList[index].date,
+                            style: const TextStyle(
+                              color: AppColors.grayscaleLabel,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                          trailing: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Text(
+                                presenceRecapEntitiesList[index].time,
+                                style: TextStyle(
+                                  color:
+                                      presenceRecapEntitiesList[index].status ==
+                                              'Hadir'
+                                          ? AppColors.stateSuccessBase
+                                          : AppColors.stateErrorBase,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              Text(
+                                presenceRecapEntitiesList[index].status,
+                                style: const TextStyle(
+                                  color: AppColors.grayscaleLabel,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _ChartData {
+  _ChartData(this.x, this.y, this.pointColorMapper);
+
+  final String x;
+  final double y;
+  final Color? pointColorMapper;
+}
