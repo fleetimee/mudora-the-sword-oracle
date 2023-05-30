935705b1f92f9c0b769fb5a0878cd1b822ebda8a
Novian Andika <novian.andika@students.amikom.ac.id>
2023-05-30 11:47:21 +0700

    🎉 feat(presence_ui.dart): add ConfigurableExpansionTileNullSafety package The ConfigurableExpansionTileNullSafety package is now imported and can be used in the presence_ui.dart file. This package provides an expansion tile widget that can be customized to fit the needs of the application.
    🎨 style(presence_ui.dart): add collapsible summary section
    This commit adds a collapsible summary section to the Presence page. The summary section contains a dropdown menu to select a month, followed by a row of PresenceStatus widgets that display the attendance status for the selected month. The summary section can be expanded or collapsed by clicking on the arrow icon.
    
    ✨ feat(presence_ui.dart): add new widget for presence status
    This commit adds a new widget called PresenceStatus which is used to display the status of a user's presence. The widget takes in several parameters such as status, statusColor, progress, days, percentage, and width. These parameters are used to customize the appearance of the widget. The widget displays the status, the number of days, and the percentage of time the user has been present. It also displays a progress bar that shows the user's progress towards their goal.

--- lib/ui/pages/presence/presence_ui.dart
+++ lib/ui/pages/presence/presence_ui.dart
@@ -1,3 +1,4 @@
+import 'package:configurable_expansion_tile_null_safety/configurable_expansion_tile_null_safety.dart';
 import 'package:fleetime_hris/common/constant/color_constant.dart';
 import 'package:fleetime_hris/common/constant/string_constant.dart';
 import 'package:fleetime_hris/data/entities/presence_card_entieties.dart';
@@ -21,6 +22,340 @@ class PresencePage extends StatelessWidget {
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
+              const SizedBox(
+                height: 25.0,
+              ),
+              ConfigurableExpansionTile(
+                header: Container(
+                  height: 200,
+                  width: 320,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8.0),
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 0.5,
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Expanded(
+                              flex: 2,
+                              child: Text(
+                                'Ringkasan',
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.w700,
+                                  color: AppColors.grayscaleTitle,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 30.0,
+                            ),
+                            DropdownMenu(
+                              width: 120.0,
+                              inputDecorationTheme: InputDecorationTheme(
+                                border: InputBorder.none,
+                                filled: false,
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: const BorderSide(
+                                    color: Colors.grey,
+                                    width: 0.5,
+                                  ),
+                                  borderRadius: BorderRadius.circular(8.0),
+                                ),
+                              ),
+                              initialSelection: 'April 2023',
+                              dropdownMenuEntries: const [
+                                DropdownMenuEntry(
+                                  value: 'April 2023',
+                                  label: 'April 2023',
+                                ),
+                                DropdownMenuEntry(
+                                  value: 'April 2022',
+                                  label: 'April 2022',
+                                ),
+                                DropdownMenuEntry(
+                                  value: 'April 2021',
+                                  label: 'April 2021',
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 16.0,
+                        ),
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            PresenceStatus(
+                              days: '12',
+                              status: 'Hadir',
+                              percentage: '100%',
+                              progress: 1.0,
+                              statusColor: Colors.green,
+                            ),
+                            PresenceStatus(
+                              days: '12',
+                              status: 'Izin',
+                              percentage: '70%',
+                              progress: 0.7,
+                              statusColor: Colors.yellow,
+                            ),
+                            PresenceStatus(
+                              days: '0',
+                              status: 'Sakit',
+                              percentage: '1%',
+                              progress: 1.0,
+                              statusColor: Colors.red,
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 8.0,
+                        ),
+                        const Divider(
+                          color: Colors.grey,
+                          thickness: 0.5,
+                          height: 0.0,
+                        ),
+                        const SizedBox(
+                          height: 8.0,
+                        ),
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('Lainnya'),
+                            Icon(
+                              Icons.arrow_forward_ios,
+                              size: 18.0,
+                              color: AppColors.grayscaleBody,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                headerExpanded: Container(
+                  height: 470,
+                  width: 320,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8.0),
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 0.5,
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Expanded(
+                              flex: 2,
+                              child: Text(
+                                'Ringkasan',
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.w700,
+                                  color: AppColors.grayscaleTitle,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 30.0,
+                            ),
+                            DropdownMenu(
+                              width: 120.0,
+                              inputDecorationTheme: InputDecorationTheme(
+                                border: InputBorder.none,
+                                filled: false,
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: const BorderSide(
+                                    color: Colors.grey,
+                                    width: 0.5,
+                                  ),
+                                  borderRadius: BorderRadius.circular(8.0),
+                                ),
+                              ),
+                              initialSelection: 'April 2023',
+                              dropdownMenuEntries: const [
+                                DropdownMenuEntry(
+                                  value: 'April 2023',
+                                  label: 'April 2023',
+                                ),
+                                DropdownMenuEntry(
+                                  value: 'April 2022',
+                                  label: 'April 2022',
+                                ),
+                                DropdownMenuEntry(
+                                  value: 'April 2021',
+                                  label: 'April 2021',
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 16.0,
+                        ),
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            PresenceStatus(
+                              days: '12',
+                              status: 'Hadir',
+                              percentage: '100%',
+                              progress: 1.0,
+                              statusColor: Colors.green,
+                            ),
+                            PresenceStatus(
+                              days: '12',
+                              status: 'Sakit',
+                              percentage: '70%',
+                              progress: 0.7,
+                              statusColor: Colors.yellow,
+                            ),
+                            PresenceStatus(
+                              days: '0',
+                              status: 'Alpha',
+                              percentage: '1%',
+                              progress: 1.0,
+                              statusColor: Colors.red,
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 16.0,
+                        ),
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            PresenceStatus(
+                              days: '12',
+                              width: 120,
+                              status: 'Izin',
+                              percentage: '100%',
+                              progress: 1.0,
+                              statusColor: Colors.green,
+                            ),
+                            VerticalDivider(
+                              color: Colors.red,
+                              thickness: 0.5,
+                              width: 1.0,
+                            ),
+                            PresenceStatus(
+                              days: '12',
+                              width: 120,
+                              status: 'Cuti',
+                              percentage: '100%',
+                              progress: 1.0,
+                              statusColor: Colors.green,
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 16.0,
+                        ),
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            PresenceStatus(
+                              days: '12',
+                              width: 120,
+                              status: 'Lembur',
+                              percentage: '100%',
+                              progress: 1.0,
+                              statusColor: Colors.green,
+                            ),
+                            VerticalDivider(
+                              color: Colors.red,
+                              thickness: 0.5,
+                              width: 1.0,
+                            ),
+                            PresenceStatus(
+                              days: '12',
+                              width: 120,
+                              status: 'Terlambat',
+                              percentage: '100%',
+                              progress: 1.0,
+                              statusColor: Colors.green,
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 16.0,
+                        ),
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            PresenceStatus(
+                              days: '12',
+                              width: 120,
+                              status: 'Pulang Cepat',
+                              percentage: '100%',
+                              progress: 1.0,
+                              statusColor: Colors.green,
+                            ),
+                            VerticalDivider(
+                              color: Colors.red,
+                              thickness: 0.5,
+                              width: 1.0,
+                            ),
+                            PresenceStatus(
+                              days: '12',
+                              width: 120,
+                              status: 'Tidak Absen',
+                              percentage: '100%',
+                              progress: 1.0,
+                              statusColor: Colors.green,
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 16.0,
+                        ),
+                        const Divider(
+                          color: Colors.grey,
+                          thickness: 0.5,
+                          height: 1.0,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text(
+                              'Ciutkan',
+                            ),
+                            IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.keyboard_arrow_up,
+                                color: AppColors.grayscaleTitle,
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 16.0,
+              ),
               ListView.separated(
                 itemCount: presenceCardEntities.length,
                 shrinkWrap: true,
@@ -84,6 +419,66 @@ class PresencePage extends StatelessWidget {
       ),
       bottomNavigationBar: FleetimeBottomNavBar(
         index: selectedIndex,
+      ),
+    );
+  }
+}
+
+class PresenceStatus extends StatelessWidget {
+  const PresenceStatus({
+    super.key,
+    required this.status,
+    required this.statusColor,
+    required this.progress,
+    required this.days,
+    required this.percentage,
+    this.width,
+  });
+
+  final String status;
+  final Color statusColor;
+  final double progress;
+  final String days;
+  final String percentage;
+  final double? width;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width ?? 80,
+      height: 60.0,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            status,
+            style: const TextStyle(
+              fontSize: 14.0,
+              fontWeight: FontWeight.w700,
+              color: AppColors.grayscaleTitle,
+            ),
+          ),
+          const SizedBox(
+            height: 8.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('$days hari'),
+              Text(percentage),
+            ],
+          ),
+          const SizedBox(
+            height: 8.0,
+          ),
+          LinearProgressIndicator(
+            value: progress,
+            backgroundColor: AppColors.grayscaleBackground,
+            valueColor: AlwaysStoppedAnimation<Color>(
+              statusColor,
+            ),
+          ),
+        ],
       ),
     );
   }
